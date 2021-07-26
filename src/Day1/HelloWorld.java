cf4a50913e4ff21c3839184980c068eb6c521519
SHOKHZODJURAEV <shahaking9393@gmail.com>
2021-07-26 15:48:13 -0400

    changes 4

--- src/Day1/HelloWorld.java
+++ src/Day1/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("How are you, World?");
         System.out.println(" I am fine");
+        System.out.println("how old are you?");
 
     }
 }
