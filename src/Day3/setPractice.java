2b32cbb10df6682c3d0f55351b35d1d33af9f9b7
SHOKHZODJURAEV <shahaking9393@gmail.com>
2021-07-29 14:49:11 -0400

    cool

---
+++ src/Day3/setPractice.java
@@ -0,0 +1,11 @@
+package Day3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class setPractice {
+    public static void main(String[] args) {
+
+      //  ArrayList<Integer> number = new ArraysList<>();
+    }
+}
