63c63f31b75747bd69ba4431b1d8624e7b40ad5e
SHOKHZODJURAEV <shahaking9393@gmail.com>
2021-07-29 14:36:11 -0400

    continue

--- src/Day3/ListPractice.java
+++ src/Day3/ListPractice.java
@@ -1,4 +1,13 @@
 package Day3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListPractice {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+        // adding information
+    }
 }
