601a30c4ca92e5c6f2fb9328fa67366b2c00fc64
SHOKHZODJURAEV <shahaking9393@gmail.com>
2021-07-29 16:22:13 -0400

    added on line 10

--- src/Day3/movie.java
+++ src/Day3/movie.java
@@ -3,5 +3,7 @@ package Day3;
 public class movie {
     // watching 10 more minutes
     // watching 25 min
+    // to continue the movie
+    // Added some awesome functionality into movie class
 
 }
