7d609f80a837051be9b417ac18bebc51d16a230b
SHOKHZODJURAEV <shahaking9393@gmail.com>
2021-07-30 11:32:59 -0400

    Started with tc

--- src/Day3/movie.java
+++ src/Day3/movie.java
@@ -1,10 +1,5 @@
 package Day3;
 
-public class movie {
-    // watching 10 more minutes
-    // watching 25 min
-    // to continue the movie
-    // Added some awesome functionality into movie class
-    // done with tc100
-
+public class Movie {
+    // the spouse came in and watches the minute until 20 mins
 }
