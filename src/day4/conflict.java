4d5a7a2e4b1c18e85aab4649e15ec1cfc9311783
SHOKHZODJURAEV <shahaking9393@gmail.com>
2021-07-30 10:49:56 -0400

    Master collection 2 is done

--- src/day4/conflict.java
+++ src/day4/conflict.java
@@ -1,10 +1,9 @@
 package day4;
 
 public class conflict {
-<<<<<<< HEAD
+
     // his is master branch content
-=======
-I can type anything what I want
-    // this is collection branch content
->>>>>>> collection
+
+//I can type anything what I want
+// this is merge collection2 branch does not kmow
 }
